421f80ac10b49012fa9999bbfcfeec84c1855231
Darcy Harrison <darcyharrison@thinkboxsoftware.com>
2013-04-23 12:11:45 -0500

    Added documentation to the interface of class any

--- prtio/detail/any.hpp
+++ prtio/detail/any.hpp
@@ -25,31 +25,72 @@
 
 namespace prtio{ namespace detail{
 
+/**
+ * This class is designed to be a type-safe way to hold a value of any type. The template function get() will throw exceptions if the held type does not match
+ * the stored type, while the get_ptr() function will return NULL if the stored type does not match.
+ */
 class any{
 public:
+	/**
+	 * Default constructor leaves the object empty (ie. this->empty() returns true).
+	 */
 	any();
 	
+	/**
+	 * Copies an any object by doing a deep copy of the contained value
+	 */
 	any( const any& rhs );
 	
+	/**
+	 * Copies an any object by doing a deep copy of the contained value
+	 */
 	any& operator=( const any& rhs );
 	
+	/**
+	 * Returns true if the has not had a value assigned.
+	 */
 	bool empty() const;
 	
+	/**
+	 * Assigns a new value to the object. A copy of the passed object is made.
+	 * \tparam T Any type with a valid copy constructor.
+	 * \param val The new value to copy and store.
+	 */
 	template <class T>
 	void set( const T& val );
 	
+	/**
+	 * Interprets the stored object as a T and returns a reference to it. Throws a any::bad_cast exception if the stored value is not actually a T.
+	 * \pre this->empty() == false
+	 * \tparam T The type to retrieve the object as. Must exactly match the type used with a previous call to set().
+	 * \return A reference to the contained value.
+	 */
 	template <class T>
 	T& get();
 	
+	/**
+	 * \overload This is a const version of get()
+	 */
 	template <class T>
 	const T& get() const;
 	
+	/**
+	 * Interprets the stored object as a T and returns a pointer to it. Returns a NULL pointer if the stored value is not a T.
+	 * \tparam T the type to retrieve the object as.
+	 * \return A pointer to the contained T value, or NULL if a T value is not held.
+	 */
 	template <class T>
 	T* get_ptr();
 	
+	/**
+	 * \overload
+	 */
 	template <class T>
 	const T* get_ptr() const;
 	
+	/**
+	 * Swaps the contained values of two any objects.
+	 */
 	void swap( any& rhs );
 	
 	/** 
@@ -63,6 +104,9 @@ public:
 	 */
 	void write( std::ostream& ostream ) const;
 	
+	/**
+	 * The type thrown when get<T>() is called and the requested type does not match the held type.
+	 */
 	class bad_cast;
 	
 private:
@@ -83,6 +127,9 @@ public:
 	}
 };
 
+/**
+ * This swap function should be found via ADL when called without explicit namespaces on any objects.
+ */
 inline void swap( any& lhs, any& rhs ){
 	lhs.swap( rhs );
 }
